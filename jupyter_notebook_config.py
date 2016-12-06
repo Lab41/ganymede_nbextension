2db57a279c0d45279881dd127c54a3bc67e3751e
kylemvz <kylez@lab41.org>
2016-12-06 11:33:15 -0800

    Updated documentation and added a feature to include active notebook filepath in generated JSON log messages.

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,10 +1,21 @@
-c.NotebookApp.server_extensions = [
-    'ganymede.ganymede',
-]
+# server_extensions is deprecated, use nbserver_extensions instead (below).
+# c.NotebookApp.server_extensions = [
+#     'ganymede.ganymede',
+# ]
+# This line activates the Ganymede extension.
+c.NotebookApp.nbserver_extensions = {
+    'ganymede.ganymede':'ganymede.ganymede',
+}
 
 """
-# To include custom logging handlers on the GanymedeHandler request handler.
+# To append the current notebook path in the generated JSON message as a key-value pair:
+# K = "filepath", V = relative path to notebook from Jupyter homedir.
 from ganymede.ganymede import GanymedeHandler
+GanymedeHandler.include_filepath = True
+"""
+
+"""
+# To overwrite the default logging handler, i.e. logging.StreamHandler(stream=sys.stdout)
 import logging
 GanymedeHandler.handlers = [
     logging.NullHandler(),
